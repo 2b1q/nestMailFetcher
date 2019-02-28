1bd0fe0208fe848f2e678d645d06db598af169b2
2b3q <2bb3qq@gmail.com>
2019-02-28 18:36:00 +0300

    refactoring RPC messaging to Observable pattern

--- src/rpc-client.service.ts
+++ src/rpc-client.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { Client, ClientProxy, Transport } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class RpcClientService implements OnModuleInit {
@@ -7,8 +8,39 @@ export class RpcClientService implements OnModuleInit {
   @Client({ transport: Transport.REDIS })
   client: ClientProxy;
 
-  // test
+  // RedisRPC sender Observable
+  private sender: Observable<any>;
+
+  // Msg counter
   private counter: number = 0;
+
+  // Msg RPC msg wrapper
+  private emit() {
+    // construct data payload
+    const data = {
+      fromService: 'nestMail.fetcher.RpcClientService',
+      payload: this.counter,
+    };
+    // add RPC msg pattern
+    const pattern = { cmd: 'pong' };
+    // log RPC.send data
+    this.logger.warn(
+      `Send ${JSON.stringify({
+        payload: data.payload,
+      })} MsgPattern ${JSON.stringify(pattern)}`,
+    );
+    // hook up RPC client Observer
+    this.sender = this.client.send(pattern, data);
+    // subscribe to event => send RPC msg
+    // () => handle next (RPC response)
+    // () => handle Errors
+    // () => handle Observer complete
+    this.sender.subscribe(
+      next => this.logger.log(`next observer data: ${JSON.stringify(next)}`),
+      err => this.logger.error(`err observer handler: ${JSON.stringify(err)}`),
+      () => this.logger.warn(`complete observer handler ${++this.counter}`),
+    );
+  }
 
   // module initialization hook
   async onModuleInit() {
@@ -18,27 +50,12 @@ export class RpcClientService implements OnModuleInit {
       `onModuleInit hook initialized => client connected to Redis server`,
     );
 
-    // test RPC TX
-    setInterval(() => {
-      this.counter++;
-      // send RPC CMD from service
-      this.txRpc(
-        { cmd: 'pong' },
-        {
-          fromService: 'nestMail.fetcher.RpcClientService',
-          payload: this.counter,
-        },
-      );
-    }, 3000);
+    // emit RPC msg every 2000ms
+    setInterval(() => this.emit(), 2000);
   }
 
-  // RPC message sender wrapper
+  // RPC message sender wrapper (with Promise)
   async txRpc(pattern: any, data: any) {
-    this.logger.log(
-      `
-      Send data: ${JSON.stringify(data)}
-      to message handler pattern ${JSON.stringify(pattern)}`,
-    );
     // Send data to all listening to product_created
     await this.client.send(pattern, data).toPromise();
   }
